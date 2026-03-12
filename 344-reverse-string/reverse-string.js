592152560bbd562b66f110bb7660c07f136bf384
Bharath Kumar <60884854+Spidey023@users.noreply.github.com>
2026-03-11 20:58:04 -0400

    Time: 0 ms (100.00%) | Memory: 61.1 MB (67.57%) - LeetSync

---
+++ 344-reverse-string/reverse-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let start=0;
+    let end=s.length-1;
+    let temp=0;
+
+    while(start<end){
+        temp=s[start]
+        s[start]=s[end]
+        s[end] = temp
+
+        start++
+        end--
+    }
+    return s
+};
