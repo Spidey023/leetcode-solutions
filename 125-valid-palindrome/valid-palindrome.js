7804bcb5624170cd6ffa563784acf70c3693a470
Bharath Kumar <60884854+Spidey023@users.noreply.github.com>
2026-03-11 21:25:30 -0400

    Time: 3 ms (86.07%) | Memory: 57.6 MB (66.74%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,17 @@
+var isPalindrome = function(s) {
+
+    let str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    let start = 0;
+    let end = str.length - 1;
+
+    while(start < end){
+        if(str[start] !== str[end]){
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
+};
