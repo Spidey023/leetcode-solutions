31a2f64051820ff1f19eb227e088d1fcd529127a
Bharath Kumar <60884854+Spidey023@users.noreply.github.com>
2026-03-12 20:20:51 -0400

    Time: 0 ms (100.00%) | Memory: 60.9 MB (32.29%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+   let slow=0
+ 
+
+   for(let fast=0;fast<nums.length;fast++){
+   
+    if(nums[fast]!==0){
+        let temp=nums[fast]
+        nums[fast]=nums[slow]
+        nums[slow]=temp
+        slow++
+    }
+    }
+        return nums
+   }
