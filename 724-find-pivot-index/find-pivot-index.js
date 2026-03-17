248632a0c7ac33812cf65d194dd96483802d799c
Bharath Kumar <60884854+Spidey023@users.noreply.github.com>
2026-03-17 12:30:38 -0400

    Time: 3 ms (50.03%) | Memory: 56.4 MB (86.36%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let leftSum=0
+    let total=0
+
+
+   for(let j=0;j<nums.length;j++){
+    total+=nums[j]
+   }
+
+    for(let i=0;i<nums.length;i++){
+       
+
+        let rightSum=total - leftSum - nums[i]
+         
+          
+        if(leftSum===rightSum){
+            return i
+        }
+leftSum+=nums[i]
+    }
+    return -1
+};
